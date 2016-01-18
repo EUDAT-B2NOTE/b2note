ec1e2821f83bf890d860ebaf219520b539a7a8f2
ant1b <antoine@Admins-MacBook-Pro.local>
2016-01-18 13:00:18 +0100

    Display annotation info in html table

--- searchapp/models.py
+++ searchapp/models.py
@@ -30,3 +30,47 @@ class Provenance(models.Model):
 class Annotation(models.Model):
 	triple 		= EmbeddedModelField( 'Triple' )
 	provenance 	= EmbeddedModelField( 'Provenance' )
+
+
+'''
+from searchapp import models
+loadup_info = [\
+	("http://purl.obolibrary.org/obo/NCBITaxon_3262","Dutch rush"),\
+	("http://purl.obolibrary.org/obo/CHEBI_27789","Dutch liquid"),\
+	("http://purl.obolibrary.org/obo/NCBITaxon_158174","Dutch grass"),\
+	("http://purl.obolibrary.org/obo/NCBITaxon_267265","Dutch eggplant"),\
+	("http://www.orpha.net/ORDO/Orphanet_100006","Hereditary cerebral hemorrhage with amyloidosis, Dutch type"),\
+	("http://purl.obolibrary.org/obo/GEO_000000229","Sint Maarten (Dutch part)"),\
+	("http://purl.obolibrary.org/obo/NCBITaxon_283010","Phytophthora hybrid Dutch variant"),\
+	("http://purl.obolibrary.org/obo/NCBITaxon_35876","Dutch iris")]
+for tupl in loadup_info:
+	models.Annotation(\
+		triple=models.Triple(\
+			subject=models.TripleElement(iri="https://b2share.eudat.eu/record/30",label="Orthography-based dating and localisation of Middle Dutch charters", definition="",curation_status="", ontology_iri="",ontology_shortname="",ontology_version="",),\
+			predicate=models.TripleElement(iri="http://purl.obolibrary.org/obo/IAO_0000136",label="is about",definition="Is_about is a (currently) primitive relation that relates an information artifact to an entity.",curation_status="pending final vetting",ontology_iri="http://purl.obolibrary.org/obo/iao.owl",ontology_shortname="IAO",ontology_version="2015,02,23",),\
+			object=models.TripleElement(\
+				iri=tupl[0],\
+				label=tupl[1],
+				definition="",
+				curation_status="",
+				ontology_iri="",
+				ontology_shortname="",\
+				ontology_version="",\
+			),\
+		),\
+		provenance=models.Provenance(\
+		  createdBy="abremaud@esciencefactory.com",
+		),\
+	).save()
+
+'''
+#models.Annotation(\
+    # triple=models.Triple(\
+    #   subject=models.TripleElement(iri='https://b2share.eudat.eu/record/30'), \
+    # predicate=models.TripleElement(iri='http://purl.obolibrary.org/obo/IAO_0000136', \
+    # label='is about', \
+    # definition='Is_about is a (currently) primitive relation that relates an information artifact to an entity.', \
+    # curation_status='pending final vetting', \
+    # ontology_iri='http://purl.obolibrary.org/obo/iao.owl', \
+    # ontology_shortname='IAO', ontology_version = '2015,02,23'), \
+    # object=models.TripleElement(iri='http://purl.obolibrary.org/obo/NCBITaxon_3262',label='Dutch rush')), provenance=models.Provenance( createdBy = 'abremaud@esciencecfactory.com' )).save()
