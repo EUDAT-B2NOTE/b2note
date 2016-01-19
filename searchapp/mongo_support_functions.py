cca0c790703a1a109763449157797788b5e5ee08
ant1b <antoine@Admins-MacBook-Pro.local>
2016-01-19 13:54:50 +0100

    Object iri as string not list
    Repairs linking to external resources.

--- searchapp/mongo_support_functions.py
+++ searchapp/mongo_support_functions.py
@@ -1,7 +1,7 @@
 import json
 
-
 from .models import *
+
 
 
 
@@ -16,14 +16,14 @@ def CreateFromPOSTinfo( subject_url, object_json ):
             o = json.loads(object_json)
 
             if "uris" in o.keys():
-                object_uri = o["uris"]
+                object_uri = o["uris"][0]
                 if "labels" in o.keys(): object_label = o["labels"]
 
                 print object_label, " ", object_uri
 
                 ann = Annotation(\
                         triple=Triple(\
-                                subject=TripleElement(iri=subject_url,label="", definition="",curation_status="", ontology_iri="",ontology_shortname="",ontology_version="",),\
+                                subject=TripleElement(iri=subject_url, label="", definition="",curation_status="", ontology_iri="",ontology_shortname="",ontology_version="",),\
                                 predicate=TripleElement(iri="http://purl.obolibrary.org/obo/IAO_0000136",label="is about",definition="Is_about is a (currently) primitive relation that relates an information artifact to an entity.",curation_status="pending final vetting",ontology_iri="http://purl.obolibrary.org/obo/iao.owl",ontology_shortname="IAO",ontology_version="2015,02,23",),\
                                 object=TripleElement(\
                                         iri     =   object_uri,\
@@ -43,7 +43,7 @@ def CreateFromPOSTinfo( subject_url, object_json ):
     except ValueError:
 
         print "Could not save to DB"
-        return Annotation.objects.all()
+        return False
 
     print "Created an Annotation"
-    return Annotation.objects.all()
+    return True
