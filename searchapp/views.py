5cf33544c1a0db8dfc50c126ca0832e7228e61ea
ant1b <antoine@Admins-MacBook-Pro.local>
2016-01-20 15:53:13 +0100

    Delete_annotation view

--- searchapp/views.py
+++ searchapp/views.py
@@ -15,6 +15,33 @@ def hostpage(request):
     return render(request, 'searchapp/hostpage.html', {'iframe_on': 350})
 
 
+
+# forbidden CSRF verification failed. Request aborted.
+@csrf_exempt
+def delete_annotation(request):
+
+    annotation_list = Annotation.objects.all()
+
+    if request.POST.get('ontology_json'):
+        annotation_list = DeleteFromPOSTinfo( request.POST.get('ontology_json') )
+
+    subject_tofeed = ""
+    if request.POST.get('subject_tofeed')!=None:
+        subject_tofeed = request.POST.get('subject_tofeed')
+
+    pid_tofeed = ""
+    if request.POST.get('pid_tofeed')!=None:
+        pid_tofeed = request.POST.get('pid_tofeed')
+
+    context = RequestContext(request, {
+        'annotation_list': annotation_list,
+        'subject_tofeed': subject_tofeed,
+        'pid_tofeed': pid_tofeed,
+    })
+    return render_to_response('searchapp/interface_main.html', context)
+
+
+
 # forbidden CSRF verification failed. Request aborted.
 @csrf_exempt
 def create_annotation(request):
