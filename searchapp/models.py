1cb3d6eefe6b9f6277deeebad4671ce32c48f9d9
ant1b <antoine@Admins-MacBook-Pro-2.local>
2016-07-06 14:01:38 +0200

    No "@" with id and type
    Necessary linking catered for within context.

--- searchapp/models.py
+++ searchapp/models.py
@@ -5,7 +5,7 @@ from django_mongodb_engine.contrib import MongoDBManager
 
 
 class Agent(models.Model):
-	jsonld_id 	    = models.CharField( max_length = 4096, null=True ) #"https://b2note.bsc.es/agent/" + mongo_uid
+	id 	    = models.CharField( max_length = 4096, null=True ) #"https://b2note.bsc.es/agent/" + mongo_uid
 	PERSON          = 'Human agent'
 	ORGANISATION    = 'Organization agent'
 	SOFTWARE        = 'Software agent'
@@ -14,7 +14,7 @@ class Agent(models.Model):
         (ORGANISATION,	'Organization'),	# foaf:Organization
         (SOFTWARE, 		'Software'),		# prov:SoftwareAgent
     )
-	jsonld_type	= SetField( models.CharField( max_length = 32,\
+	type	= SetField( models.CharField( max_length = 32,\
 												 choices=AGENT_CHOICES), null=True )
 	name		= models.CharField( max_length = 2048, null=True ) #foaf:name
 	nick	    = models.CharField( max_length = 2048, null=True ) #foaf:nick
@@ -23,51 +23,51 @@ class Agent(models.Model):
 
 
 class List(models.Model):
-	jsonld_type	= models.CharField( max_length = 32, \
+	type	= models.CharField( max_length = 32, \
 									   choices = (("List multiplicity construct","List"),) )	# oa:List
 	members		= ListField( EmbeddedModelField() ) # oa:memberList
 
 
 class Composite(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Composite multiplicity construct","Composite"),) )	# oa:Composite
 	item		= ListField( EmbeddedModelField() ) # oa:item
 
 
 class Choice(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Choice multiplicity construct","Choice"),) )	# oa:Choice
 	members		= ListField( EmbeddedModelField() ) # oa:memberList
 
 
 class CssStyleSheet(models.Model):
-	jsonld_id 		= models.CharField( max_length = 4096, null=True )
+	id 		= models.CharField( max_length = 4096, null=True )
 	CSS 			= "CSS style sheet"
 	EMBEDDED		= "Embedded content"
 	CLASS_CHOICE	= (
 		(CSS, 		"CssStylesheet"),	# oa:CssStyle
 		(EMBEDDED,	"EmbeddedContent"),	# oa:EmbeddedContent
 	)
-	jsonld_type = SetField( models.CharField( max_length = 32, choices=CLASS_CHOICE) ) # oa:CssStyle
+	type = SetField( models.CharField( max_length = 32, choices=CLASS_CHOICE) ) # oa:CssStyle
 	value		= models.TextField() # rdf:value
 	format		= SetField( models.CharField( max_length = 256 ), null=True )  # dc:format, [rfc6838]
 
 
 class RequestHeaderState(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("HTTP request state","HttpState"),) )	# oa:HttpRequestState
 	value		= models.TextField() # rdf:value
 
 
 class TimeState(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Time state","TimeState"),) )	# oa:TimeState
 	sourceDate	= SetField( models.DateTimeField(), null=True ) # oa:sourceDate, MUST xsd:dateTime SHOULD timezone.
 	cached		= SetField( models.CharField( max_length = 4096 ), null=True ) # oa:cachedSource
 
 
 class DataPositionSelector(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Data position selector","DataPositionSelector"),) )	# oa:DataPositionSelector
 	start		= models.PositiveIntegerField() # oa:start
 	end			= models.PositiveIntegerField()	# oa:end
@@ -80,14 +80,14 @@ class SvgSelector(models.Model):
 		(SVG, 		"SvgSelector"),		# oa:SvgSelector
 		(EMBEDDED,	"EmbeddedContent"),	# oa:EmbeddedContent
 	)
-	jsonld_type	= SetField( models.CharField( max_length = 32, choices = SVG_SELECTOR_TYPE ) )
+	type	= SetField( models.CharField( max_length = 32, choices = SVG_SELECTOR_TYPE ) )
 	text		= models.TextField( null=True ) # oa:text
 	format		= models.CharField( max_length = 32,\
 									  choices = (("SVG media-type","image/svg+xml"),), null=True ) # dc:format
 
 
 class TextPositionSelector(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Text position selector","TextPositionSelector"),) )	# oa:TextPositionSelector
 	start		= models.PositiveIntegerField() # oa:start
 	# [0:2147483647] i.e. with upper-limit 16 bytes per character, max file size of 17179869176 bytes ~ 17 Gb
@@ -95,7 +95,7 @@ class TextPositionSelector(models.Model):
 
 
 class TextQuoteSelector(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Text quote selector","TextQuoteSelector"),) )	# oa:TextQuoteSelector
 	exact		= models.TextField() # oa:exact
 	prefix		= models.CharField( max_length = 2048, null=True ) # oa:prefix
@@ -103,15 +103,15 @@ class TextQuoteSelector(models.Model):
 
 
 class FragmentSelector(models.Model):
-	jsonld_type	= models.CharField( max_length = 32,\
+	type	= models.CharField( max_length = 32,\
 									   choices = (("Fragment selector","FragmentSelector"),), null=True )	# oa:FragmentSelector
 	value		= models.CharField( max_length = 4096 )	# rdf:value
 	conformsTo	= models.CharField( max_length = 256 )	# dcterms:conformsTo
 
 
 class SpecificResource(models.Model):
-	jsonld_id 	= models.CharField( max_length = 4096, null=True )
-	jsonld_type	= models.CharField( max_length = 64, null=True ) # (rdf:type) oa:SpecificResource
+	id 	= models.CharField( max_length = 4096, null=True )
+	type	= models.CharField( max_length = 64, null=True ) # (rdf:type) oa:SpecificResource
 	source		= EmbeddedModelField("ExternalResource") # (oa:hasSource)
 	BOOKMARKING     = "bookmarking"
 	CLASSIFYING     = "classifying"
@@ -149,8 +149,8 @@ class SpecificResource(models.Model):
 
 
 class TextualBody(models.Model):
-	jsonld_id 	= models.CharField( max_length = 4096, null=True ) #"https://b2note.bsc.es/textualbody/" + mongo_uid
-	jsonld_type	= SetField( models.CharField( max_length = 64 ), null=True ) # rdf:type; oa:TextualBody
+	id 	= models.CharField( max_length = 4096, null=True ) #"https://b2note.bsc.es/textualbody/" + mongo_uid
+	type	= SetField( models.CharField( max_length = 64 ), null=True ) # rdf:type; oa:TextualBody
 	text        = models.TextField() # oa:text
 	language 	= SetField( models.CharField( max_length = 256 ), null=True )  # dc:language, [rfc5646]
 	format		= SetField( models.CharField( max_length = 256 ), null=True )  # dc:format, [rfc6838]
@@ -189,7 +189,7 @@ class TextualBody(models.Model):
 
 
 class ExternalResource(models.Model):
-	jsonld_id 	= models.CharField( max_length = 4096, null=True )
+	id 	= models.CharField( max_length = 4096, null=True )
 	DATASET = "dataset"
 	IMAGE   = "image"
 	VIDEO   = "video"
@@ -202,7 +202,7 @@ class ExternalResource(models.Model):
         (SOUND,     "Audio"),	# dctypes:Sound
         (TEXT,      "Text"),	# dctypes:Text
     )
-	jsonld_type	= SetField( models.CharField( max_length = 64, choices=RESOURCE_TYPE_CHOICES), null=True ) #rdf:class
+	type	= SetField( models.CharField( max_length = 64, choices=RESOURCE_TYPE_CHOICES), null=True ) #rdf:class
 	language 	= ListField( models.CharField( max_length = 256 ), null=True )  # dc:language, [rfc5646]
 	format		= ListField( models.CharField( max_length = 256 ), null=True )  # dc:format, [rfc6838]
 	creator 	= ListField( EmbeddedModelField("Agent"), null=True )   # dcterms:creator
@@ -210,8 +210,8 @@ class ExternalResource(models.Model):
 
 
 class Annotation(models.Model):
-	jsonld_id   = models.CharField( max_length = 4096, null = True )
-	jsonld_type = SetField( models.CharField( max_length = 256 ) )		# (rdf:type) oa:Annotation and others
+	id          = models.CharField( max_length = 4096, null = True )
+	type        = SetField( models.CharField( max_length = 256 ) )		# (rdf:type) oa:Annotation and others
 	body        = ListField( EmbeddedModelField(), null=True )          # CharField( max_length = 4096, null = True )
 	target      = ListField( EmbeddedModelField() )                     # models.CharField( max_length = 4096 )
 	language 	= models.CharField( max_length = 256, null=True )       # dc:language, [rfc5646]
