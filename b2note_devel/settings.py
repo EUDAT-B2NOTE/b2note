2fdca1e44f4d0a47611399d7ef7b607228eff805
Pablo Rodenas <pablo.rodenas@bsc.es>
2016-08-30 18:55:15 +0200

    Trying to work with travis 44.

--- b2note_devel/settings.py
+++ b2note_devel/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
         'NAME': os.environ['MONGODB_NAME'],                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': os.environ['MONGODB_USR'],
-        'PASSWORD': 'abc123',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
