a387b7f01503ce5c47910a64c871f1a21e7b5228
Pablo Rodenas <pablo.rodenas@bsc.es>
2016-09-01 12:45:47 +0200

    API returns the html of the interface main

--- b2note_api/b2note_api.py
+++ b2note_api/b2note_api.py
@@ -1,7 +1,20 @@
 from eve import Eve
+from flask import request
 from settings import mongo_settings
+import json, requests
 
 app = Eve(settings=mongo_settings)
+
+
+@app.route('/create_annotation', methods=['POST'])
+def post_to_django():
+    pid = request.values.get('pid_tofeed')
+    subject = request.values.get('subject_tofeed')
+    url='https://b2note.bsc.es/interface_main'
+    data = {"pid_tofeed": str(pid), "subject_tofeed": str(subject) }
+    headers = {'Content-Type':'application/json'}
+    r = requests.post(url, data=data)
+    return str(r.text)
 
 if __name__ == '__main__':
     app.run()
--- b2note_api/settings.py
+++ b2note_api/settings.py
@@ -6,11 +6,18 @@ mongo_settings = {
         'MONGO_DBNAME': os.environ['MONGODB_NAME'],
         'MONGO_USERNAME': os.environ['MONGODB_USR'],
         'MONGO_PASSWORD': os.environ['MONGODB_PWD'],
-        'DOMAIN': {'annotations': {'datasource' : { 'source': 'b2note_app_annotation' }}},
+        'DOMAIN': {
+            'annotations': {
+                'datasource' : {
+                    'source': 'b2note_app_annotation'
+                    },
+                #'url' : 'annotations/<regex("[a-f0-9]{24}"):annotation_id>/files',
+                }
+            },
         'RESOURCE_METHODS' : ['GET'],
         'ITEM_METHODS' : ['GET'],
         'ALLOW_UNKNOWN' : True, # http://stackoverflow.com/questions/34666941/python-eve-get-response-does-not-contain-contents-of-resource-unless-i-specify
-#        'DEBUG' : True,
-#        'INFO'  : True
+        'DEBUG' : True,
+        'INFO'  : True,
 }
 
