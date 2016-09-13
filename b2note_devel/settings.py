2e586b59dc2052a5f5e0463e03cebf1c5f620262
ant1b <antoine@Admins-MacBook-Pro-2.local>
2016-09-13 10:12:07 +0200

    Non-local settings as from upstream dev branch

--- b2note_devel/settings.py
+++ b2note_devel/settings.py
@@ -17,17 +17,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-MONGO_DATABASE_NAME = "b2note_mongodb" #os.environ['MONGODB_NAME']
+MONGO_DATABASE_NAME = os.environ['MONGODB_NAME']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django_mongodb_engine', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': "b2note_mongodb", #os.environ['MONGODB_NAME'],                      # Or path to database file if using sqlite3.
+        'NAME': os.environ['MONGODB_NAME'],                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        #'USER': os.environ['MONGODB_USR'],
-        #'PASSWORD': os.environ['MONGODB_PWD'],
-        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
+        'USER': os.environ['MONGODB_USR'],
+        'PASSWORD': os.environ['MONGODB_PWD'],
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     },
     'users' : {
         'ENGINE' : 'django.db.backends.sqlite3',
@@ -42,7 +42,6 @@ DATABASES = {
 # This will substitute the default User model
 # https://docs.djangoproject.com/en/dev/topics/auth/customizing/#substituting-a-custom-user-model
 AUTH_USER_MODEL = 'accounts.UserCred'
-AUTHENTICATION_BACKENDS = ['accounts.backends.EmailAuthBackend', ]
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -107,7 +106,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = "secret" #os.environ['B2NOTE_SECRET_KEY']
+SECRET_KEY = os.environ['B2NOTE_SECRET_KEY']
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
