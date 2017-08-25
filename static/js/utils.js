8cae156e758fd4934b529eb45bdff144bd603939
Aymeric Rodriguez <aymeric@Admins-MacBook-Pro-2.local>
2017-08-25 16:44:35 +0200

    add refresh buttons for captchas

--- static/js/utils.js
+++ static/js/utils.js
@@ -106,3 +106,31 @@ $(document).ready( function() {
         }
     }
 });
+
+/*
+* Add a refresh button to captchas
+* https://stackoverflow.com/questions/18972515/how-to-create-ajax-refresh-for-django-simple-captcha
+*/
+$(function() {
+    // Add refresh button after field (this can be done in the template as well)
+    /*
+    $('img.captcha').after(
+            $('<br><a href="#void" id="captcha-refresh-button" class="captcha-refresh">Refresh</a>')
+    );
+    */
+
+    // Click-handler for the refresh-link
+    $('.captcha-refresh').click(function(){
+        var $form = $(this).parents('form');
+        var url = location.protocol + "//" + window.location.hostname + ":"
+                  + location.port + "/captcha/refresh/";
+
+        // Make the AJAX-call
+        $.getJSON(url, {}, function(json) {
+            $form.find('input[name="captcha_0"]').val(json.key);
+            $form.find('img.captcha').attr('src', json.image_url);
+        });
+
+        return false;
+    });
+});
