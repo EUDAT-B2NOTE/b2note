b5fd600f58b3a916b3f3e74c2019b05e85b41c9e
Aymeric Rodriguez <arodriguez@esciencefactory.com>
2018-01-08 10:01:46 +0100

    Revert "Revert "aymRod_#331_OidcCompleteForAllAccounts""
    This reverts commit 6b0f7f474d7140bd56a46473b2b0aa1d2c0f7c25.
    
    revert the first revert

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,19 @@ from views import ResetPasswordRequestView, PasswordResetConfirmView
 
 urlpatterns = patterns('',
     url(r'^register$', 'accounts.views.register', name='register'),
-    url(r'^login$', 'accounts.views.login', name='login'),
+    url(r'^old_login$', 'accounts.views.old_login', name='oldlogin'),
     url(r'^logout$', 'accounts.views.logout', name='logout'),
     url(r'^profilepage$', 'accounts.views.profilepage', name='profile'),
     url(r'^feedbackpage$', 'accounts.views.feedbackpage', name='feedback'),
+
+    url(r'^auth_main', 'accounts.views.auth_main', name='auth'),
+    url(r'^auth_redirected', 'accounts.views.auth_redirected', name='redirected'),
+    url(r'^login$', 'accounts.views.login', name='login'),
+    url(r'^polling$', 'accounts.views.polling', name='polling'),
+    url(r'^abort$', 'accounts.views.abort', name='abort'),
+    url(r'^old_register$', 'accounts.views.old_register', name='old_register'),
+
+
 
     url(r'^reset_password_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(),name='reset_password_confirm'),
     # PS: url above is going to used for next section of implementation.
