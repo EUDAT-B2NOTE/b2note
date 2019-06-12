9fa2481e2ff8ebd2b83abfbac1c4172377e96220
Tomas Kulhanek <tkulhanek@esciencefactory.com>
2019-06-12 14:51:11 +0100

    BI-16 #comment UPDATE: initial e2e test and configuration

---
+++ b2note_app/test/e2e/demo.e2e.js
@@ -0,0 +1,24 @@
+import {PageObjectWelcome} from './welcome.po';
+import {PageObjectSkeleton} from './skeleton.po';
+import {config} from '../protractor.conf';
+
+describe('aurelia skeleton app', function() {
+  let poWelcome;
+  let poSkeleton;
+
+
+  beforeEach(async () => {
+    poSkeleton = new PageObjectSkeleton();
+    poWelcome = new PageObjectWelcome();
+
+    await browser.loadAndWaitForAureliaPage(`http://localhost:${config.port}`);
+  });
+
+  it('should load the page and display the initial page title', async () => {
+    await expect(await poSkeleton.getCurrentPageTitle()).toContain('B2Note');
+  });
+
+  it('should display greeting', async () => {
+    await expect(await poWelcome.getGreeting()).toBe('Create annotation');
+  });
+});
