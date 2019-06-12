ab44daf25a1d59e1f48b243a019f992c4a276958
Tomas Kulhanek <tkulhanek@esciencefactory.com>
2019-06-12 09:49:34 +0100

    BI-16 #comment UPDATE: array query controls allows modify, delete item in query

--- b2note_app/src/components/arraysearchdialog.js
+++ b2note_app/src/components/arraysearchdialog.js
@@ -15,8 +15,8 @@ export class Arraysearchdialog {
     this.annotationcomment = '';
     this.api = api;
     this.child = false;
-    this.query=[];
-    this.logic='AND';
+    this.query = [];
+    this.logic = '';
   }
 
   switchtab(tabid) {
@@ -39,42 +39,104 @@ export class Arraysearchdialog {
   }
 
   //TODO refactor to use api.query instead of this.query
-  search(){
-    console.log('search',this.active);
+  search() {
+    console.log('search', this.active);
     //based on active tab show return appropriate value
-    this.searchvalue = this.active === 'semantic'? this.annotationsemantic:
-      (this.active === 'keyword'? this.annotationkeyword: '');
+    this.searchvalue = this.active === 'semantic' ? this.annotationsemantic :
+      (this.active === 'keyword' ? this.annotationkeyword : '');
     this.searchtype = this.active;
-    let queryitem = {logic:this.query.length === 0?'':this.logic, type:this.searchtype,value:this.searchvalue}
-    if (queryitem.value!=='') this.query.push(queryitem);
+    let queryitem = {first:this.query.length==0,logic: this.query.length === 0 ? '' : this.logic, type: this.searchtype, value: this.searchvalue}
+    if (queryitem.value !== '') this.query.push(queryitem);
 
     this.api.searchQuery(this.query);
   }
 
-  createChild(){
+  createChild() {
     //determine the query value based on current tab
-    this.searchvalue = this.active === 'semantic'? this.annotationsemantic:
-    (this.active === 'keyword'? this.annotationkeyword: '');
+    this.searchvalue = this.active === 'semantic' ? this.annotationsemantic :
+      (this.active === 'keyword' ? this.annotationkeyword : '');
     this.searchtype = this.active;
     //create queryitem record with logic operator, type and value from dialog
-    let queryitem = {logic:this.query.length === 0?'':this.logic, type:this.searchtype,value:this.searchvalue}
+    let queryitem = {first:this.query.length==0,logic: this.query.length === 0 ? '' : this.logic, type: this.searchtype, value: this.searchvalue}
     this.query.push(queryitem);
     //reset dialog values
-    this.active='semantic';
-    this.annotationsemantic=this.annotationkeyword=this.annotationcomment='';
+    this.active = 'semantic';
+    this.annotationsemantic = this.annotationkeyword = this.annotationcomment = '';
     console.log('creating child');
     this.child = this.query.length > 0;
+    this.logic = 'AND';
   }
 
-  deleteChild(){
+  deleteChild() {
     console.log('deleting child');
     let queryitem = this.query.pop()
     this.active = queryitem.type;
-    console.log('deleteChild(), active: queryitem:',this.active,queryitem.logic,queryitem.type,queryitem.value)
-    this.annotationsemantic=this.annotationkeyword=this.annotationcomment='';
-    if (this.active==='semantic') this.annotationsemantic=queryitem.value;
-    if (this.active==='keyword') this.annotationkeyword=queryitem.value;
+    console.log('deleteChild(), active: queryitem:', this.active, queryitem.logic, queryitem.type, queryitem.value)
+    this.annotationsemantic = this.annotationkeyword = this.annotationcomment = '';
+    if (this.active === 'semantic') this.annotationsemantic = queryitem.value;
+    if (this.active === 'keyword') this.annotationkeyword = queryitem.value;
+    this.logic = queryitem.logic;
     this.child = this.query.length > 0;
+  }
+
+  deleteItem(item) {
+    console.log('deleting item');
+    let itemindex = this.query.indexOf(item);
+    if (itemindex > -1) {
+      //can delete from query
+      let queryitem = item;
+      this.query.splice(itemindex, 1); //remove the item from queries
+      this.query[0].logic=''
+      /*
+      //set current dialog to the delete queryitem properties
+      this.active = queryitem.type;
+      console.log('deleteChild(), active: queryitem:',this.active,queryitem.logic,queryitem.type,queryitem.value)
+      this.annotationsemantic=this.annotationkeyword=this.annotationcomment='';
+      if (this.active==='semantic') this.annotationsemantic=queryitem.value;
+      if (this.active==='keyword') this.annotationkeyword=queryitem.value;
+
+      */
+      this.child = this.query.length > 0;
+      this.query[0].first=true;
+    } else console.log('warning, item not found in queryindex', item, this.query);
+  }
+
+  /**
+   * Modifies item, splits the query array into 2 parts
+   * @param item
+   */
+  modifyItem(item) {
+    console.log('modify item');
+    if (this.modify){
+      //already modifying something else
+      this.confirmModifiedItem();
+    }
+    let itemindex = this.query.indexOf(item);
+    if (itemindex > -1) {
+      //copy elements from item
+      this.suffixquery = this.query.slice(itemindex+1);
+      //remove suffix from query
+      this.query.splice(itemindex+1,this.query.length-itemindex);
+      //now delete the last child - it will allow to edit it
+      this.deleteChild();
+      //set modify flag
+      this.modify=true;
+    }
+
+  }
+
+  /**
+   * Merges modification of an item
+   * @param item
+   */
+  confirmModifiedItem(){
+    //adds the dialog to the query
+    this.createChild();
+    //merge the rest of the suffix to the query
+    let newquery = this.query.concat(this.suffixquery);
+    this.query=newquery;
+    //set modify flag
+    this.modify=false;
   }
 
 }
--- b2note_app/src/components/dropdownsearchdialog.js
+++ b2note_app/src/components/dropdownsearchdialog.js
@@ -1,8 +1,10 @@
 import {AnnotationApi} from './annotationapi';
 import {inject} from 'aurelia-framework';
+import {customElement} from 'aurelia-templating';
 import {Arraysearchdialog} from "./arraysearchdialog";
 
 @inject(AnnotationApi)
+@customElement()
 export class Dropdownsearchdialog extends Arraysearchdialog {
   constructor(api) {
     super(api);
