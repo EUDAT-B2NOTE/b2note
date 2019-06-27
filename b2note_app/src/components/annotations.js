cbb3c9620055c66aff303eaa90ed9c9af5951df2
Tomas Kulhanek <tkulhanek@esciencefactory.com>
2019-06-27 13:52:20 +0100

    BI-30 # comment ADD: aggregate targets per annotation - shows number of targets (files)

--- b2note_app/src/components/annotations.js
+++ b2note_app/src/components/annotations.js
@@ -10,31 +10,31 @@ import {EventAggregator} from 'aurelia-event-aggregator';
 import {inject} from 'aurelia-framework';
 import {Taginfo} from "./messages";
 
-@inject(AnnotationApi,EventAggregator)
+@inject(AnnotationApi, EventAggregator)
 export class Annotations {
-  constructor(api,ea) {
-    this.api=api;
-    this.ea=ea;
+  constructor(api, ea) {
+    this.api = api;
+    this.ea = ea;
     this.showall = false;
     this.showfile = false;
     this.file = {};
-    this.file.semantic=[];
-    this.file.keyword=[];
-    this.file.comment=[];
-    this.file.tags=this.file.semantic;//reference
+    this.file.semantic = [];
+    this.file.keyword = [];
+    this.file.comment = [];
+    this.file.tags = this.file.semantic;//reference
     this.my = {};
-    this.my.semantic=[];
-    this.my.keyword=[];
-    this.my.comment=[];
-    this.my.tags=this.my.semantic;//reference
-    this.showmydetail=false;
-    this.showfiledetail=false;
+    this.my.semantic = [];
+    this.my.keyword = [];
+    this.my.comment = [];
+    this.my.tags = this.my.semantic;//reference
+    this.showmydetail = false;
+    this.showfiledetail = false;
 
   }
 
   switchAll() {
     this.showall = !this.showall;
-    if (this.showfile) this.showfile=false;
+    if (this.showfile) this.showfile = false;
     if (this.showall) {
       this.api.getAllMyAnnotationsSemantic()
         .then(data => {
@@ -53,7 +53,7 @@ export class Annotations {
 
   switchFile() {
     this.showfile = !this.showfile;
-    if (this.showall) this.showall=false;
+    if (this.showall) this.showall = false;
     if (this.showfile) {
       this.api.getAllAnnotationsFileSemantic()
         .then(data => {
@@ -70,67 +70,99 @@ export class Annotations {
     }
   }
 
-  //sets values of tags through array to find TextualValue and set it
-  setvalue(tags){
+  //sets values of tags through array to find TextualValue (last item) and set it
+  setvalue(tags) {
     for (let tag of tags) {
-      if (tag.body.items && tag.body.items.length>0) tag.body.value=tag.body.items[tag.body.items.length-1].value
-      else
-        if (tag.body.source) tag.body.value=tag.body.source;
-
+      if (tag.body.items && tag.body.items.length > 0) tag.body.value = tag.body.items[tag.body.items.length - 1].value
+      else if (tag.body.source) tag.body.value = tag.body.source;
     }
-    console.log('Annotations, tags',tags)
+    console.log('Annotations, tags', tags)
+    //aggregate targets
   }
 
-  showMySemantic(){
-    this.showmydetail=true;
-    this.showmytitle="Semantic"
-    this.my.tags=this.my.semantic;
-    this.setvalue(this.my.tags)
-  }
-  showMyKeyword(){
-    this.showmydetail=true;
-    this.showmytitle="Free-text keyword"
-    this.my.tags=this.my.keyword;
-  }
-  showMyComment(){
-    this.showmydetail=true;
-    this.showmytitle="Comment"
-    this.my.tags=this.my.comment;
-  }
-  showFileSemantic(){
-    this.showfiledetail=true;
-    this.showfiletitle="Semantic"
-    this.file.tags=this.file.semantic;
-    this.setvalue(this.file.tags)
-  }
-  showFileKeyword(){
-    this.showfiledetail=true;
-    this.showfiletitle="Free-text keyword"
-    this.file.tags=this.file.keyword;
-  }
-  showFileComment(){
-    this.showfiledetail=true;
-    this.showfiletitle="Comment"
-    this.file.tags=this.file.comment;
+
+  aggregatePerBody(tags) {
+    let tagset = {} // set of body values
+    let newtags = []
+    for (let tag of tags) {
+      //add body value
+     if (!tag.body.hasOwnProperty('value')) { // set body value from body.items[last] - or from body.source
+        if (tag.body.items && tag.body.items.length > 0) tag.body.value = tag.body.items[tag.body.items.length - 1].value
+        else if (tag.body.source) tag.body.value = tag.body.source;
+      }
+      if (tagset.hasOwnProperty(tag.body.value)) {
+        //adds target to existing
+        console.log('aggregatePerBody() adding target to existing body [body.value,target, index-tagset[body.value])',tag.body.value,tag.target,tagset);
+        newtags[tagset[tag.body.value]].target.items.push(tag.target)
+      } else {
+        //creates newtag
+        let index = newtags.push(tag) - 1;
+        tagset[tag.body.value] = index;
+        console.log('aggregatePerBody() creating target to existing body [body.value,target, index-tagset[body.value])',tag.body.value,tag.target,tagset);
+        newtags[tagset[tag.body.value]].target.items = [];
+        newtags[tagset[tag.body.value]].target.items.push(tag.target)
+      }
+    }
+    return newtags;
   }
 
-  showdetail(tag,domid){
+
+  showMySemantic() {
+    this.showmydetail = true;
+    this.showmytitle = "Semantic"
+    this.my.tags = this.aggregatePerBody(this.my.semantic);
+  }
+
+  showMyKeyword() {
+    this.showmydetail = true;
+    this.showmytitle = "Free-text keyword"
+    this.my.tags = this.my.keyword;
+  }
+
+  showMyComment() {
+    this.showmydetail = true;
+    this.showmytitle = "Comment"
+    this.my.tags = this.my.comment;
+  }
+
+  showFileSemantic() {
+    this.showfiledetail = true;
+    this.showfiletitle = "Semantic"
+    this.file.tags = this.aggregatePerBody(this.file.semantic);
+
+  }
+
+  showFileKeyword() {
+    this.showfiledetail = true;
+    this.showfiletitle = "Free-text keyword"
+    this.file.tags = this.file.keyword;
+  }
+
+  showFileComment() {
+    this.showfiledetail = true;
+    this.showfiletitle = "Comment"
+    this.file.tags = this.file.comment;
+  }
+
+  showdetail(tag, domid) {
     let taginfo = tag;
-    taginfo.domid=domid;
-    taginfo.mode='show';
+    taginfo.domid = domid;
+    taginfo.mode = 'show';
     this.ea.publish(new Taginfo(taginfo));
   }
-  edittag(tag,domid){
+
+  edittag(tag, domid) {
     let taginfo = tag;
-    taginfo.domid=domid;
-    taginfo.mode='edit';
+    taginfo.domid = domid;
+    taginfo.mode = 'edit';
     this.ea.publish(new Taginfo(taginfo));
 
   }
-  removetag(tag,domid){
+
+  removetag(tag, domid) {
     let taginfo = tag;
-    taginfo.domid=domid;
-    taginfo.mode='remove';
+    taginfo.domid = domid;
+    taginfo.mode = 'remove';
     this.ea.publish(new Taginfo(taginfo));
   }
 
