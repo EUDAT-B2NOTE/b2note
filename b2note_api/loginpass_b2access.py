187a9243a5259a40031749e83e6e5ed93858bba6
Tomas Kulhanek <tkulhanek@esciencefactory.com>
2019-07-29 14:05:13 +0100

    FIX: strip trailing slash

--- b2note_api/loginpass_b2access.py
+++ b2note_api/loginpass_b2access.py
@@ -25,11 +25,11 @@ import os
 
 def create_b2access_backend(name,b2accessurl):
     B2ACCESS_API_URL = b2accessurl.strip('/')
-    B2ACCESS_TOKEN_URL = '{b2accessurl}/oauth2/token'.format(b2accessurl=b2accessurl)
-    B2ACCESS_TOKENINFO_URL = '{b2accessurl}/oauth2/tokeninfo'.format(b2accessurl=b2accessurl)
-    B2ACCESS_AUTH_URL = '{b2accessurl}/oauth2-as/oauth2-authz'.format(b2accessurl=b2accessurl)
+    B2ACCESS_TOKEN_URL = '{b2accessurl2}/oauth2/token'.format(b2accessurl2=B2ACCESS_API_URL)
+    B2ACCESS_TOKENINFO_URL = '{b2accessurl2}/oauth2/tokeninfo'.format(b2accessurl2=B2ACCESS_API_URL)
+    B2ACCESS_AUTH_URL = '{b2accessurl2}/oauth2-as/oauth2-authz'.format(b2accessurl2=B2ACCESS_API_URL)
     B2ACCESS_USERINFO_SUFFIX='/oauth2/userinfo'
-    B2ACCESS_USERINFO_URL = '{b2accessurl}/oauth2/userinfo'.format(b2accessurl=b2accessurl)
+    B2ACCESS_USERINFO_URL = '{b2accessurl2}/oauth2/userinfo'.format(b2accessurl2=B2ACCESS_API_URL)
 
     class B2AccessAuth(OAuthBackend):
         OAUTH_TYPE = '2.0,oidc'
